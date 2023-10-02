20d73b8a511954a7cee9d4bf80af7ca10c9af109
malakehab2003 <lokaehab2003@gmail.com>
2023-10-02 23:14:32 +0300

    first try task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *new;
+	new = list;
+
+	while (new != NULL)
+	{
+		if (new->next == new)
+		{
+			return (1);
+		}
+		new = new->next;
+	}
+	return (0);
+}
