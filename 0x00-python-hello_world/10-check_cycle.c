70f236d20e736edb6bfc2bb6e54cac3c70e25338
malakehab2003 <lokaehab2003@gmail.com>
2023-10-02 23:46:03 +0300

    try and error

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,16 +5,26 @@
 
 int check_cycle(listint_t *list)
 {
-        listint_t *new;
-        new = list;
+  listint_t *new;
 
-        while (new != NULL && new->next != NULL)
-        {
-                if (new->next == new)
-                {
-                        return (1);
-                }
-                new = new->next;
-        }
-        return (0);
+  if (list == NULL) {
+    return 0;
+  }
+
+  new = list;
+
+  while (new != NULL && new->next != NULL) {
+    if (new == NULL || new->next == NULL) {
+      return 0;
+    }
+
+    if (new == new->next) {
+      return 1;
+    }
+
+    new = new->next;
+  }
+
+  return 0;
 }
+
