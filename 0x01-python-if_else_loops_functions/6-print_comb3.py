4d0e7234e9e0580197ddc73983072b6d176a1d1d
malakehab2003 <lokaehab2003@gmail.com>
2023-10-04 01:35:24 +0300

    finish task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 9):
     for num2 in range(10):
-        num2 = num + 1
+        num2 = num2 + num + 1
         if num != 8 or num2 != 9:
             print("{}{}".format(num, num2), end=", ")
         else:
