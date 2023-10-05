69e578f9a215cf4371679fab765ca175b17fea6a
malakehab2003 <lokaehab2003@gmail.com>
2023-10-05 15:55:21 +0300

    finish task  3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	
-	l = len(sys.argv) - 1
-	result = 0
-	
-	for i in range(l):
-		result += int(sys.argv[i + 1])
-	print("{}".format(result))
+    import sys
+
+    length = len(sys.argv) - 1
+    result = 0
+
+    for i in range(length):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
