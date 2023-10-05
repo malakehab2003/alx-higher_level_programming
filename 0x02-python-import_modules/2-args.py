73a4cd4ca1c46776eea9e3070401367e1bb65066
malakehab2003 <lokaehab2003@gmail.com>
2023-10-05 15:25:48 +0300

    finish task  2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import sys
 
     l = len(sys.argv) - 1
@@ -7,9 +9,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif l == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(l))
-        i = 1
-        for i in range(l):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(l):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
