bac5170510c4b8b72ea88178aab84080216f904c
malakehab2003 <lokaehab2003@gmail.com>
2023-10-08 19:42:47 +0300

    finish task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
@@ -9,7 +10,7 @@
  *
  * @head: the first point in the list to check
 */
-
+/**
 int is_palindrome(listint_t **head)
 {
 	int count = 0, i = 0, j, countcp = 0, half = 0;
@@ -43,4 +44,38 @@ int is_palindrome(listint_t **head)
 		new = new->next;
 	}
 	return (1);
+}
+*/
+int is_palindrome(listint_t **head)
+{
+    if (*head == NULL || (*head)->next == NULL)
+        return (1); // Empty or single-node list is a palindrome
+
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next; // Move fast pointer twice as fast as slow pointer
+        listint_t *next = slow->next;
+        slow->next = prev; // Reverse the first half of the list
+        prev = slow;
+        slow = next;
+    }
+
+    // Handle odd and even length cases
+    if (fast != NULL) // Odd length, skip the middle element
+        slow = slow->next;
+
+    while (slow != NULL)
+    {
+        if (prev->n != slow->n)
+            return (0); // Not a palindrome
+
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    return (1); // Palindrome
 }
