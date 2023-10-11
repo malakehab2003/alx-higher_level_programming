4b8abffc14b6f4a991c829fd7a1682ad2ce6a6d8
malakehab2003 <lokaehab2003@gmail.com>
2023-10-11 22:18:15 +0300

    finish task  101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new = []
-    for i in range(len(matrix)):
-        new_row = list(map(lambda x: x ** 2, matrix[i]))
-        new.append(new_row)
-    return new
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
