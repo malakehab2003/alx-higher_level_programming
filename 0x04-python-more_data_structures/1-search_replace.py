c35673f1265ac79437b1b9406361f39079030290
malakehab2003 <lokaehab2003@gmail.com>
2023-10-11 17:00:10 +0300

    finish task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if search < 0:
+        return my_list
+    if search > len(my_list):
+        return my_list
+    new = my_list[:]
+    new[search - 1] = replace
+    return new
