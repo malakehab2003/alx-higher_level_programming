ca371460a1ed3e0d273168d0c72561922347a651
malakehab2003 <lokaehab2003@gmail.com>
2023-10-11 17:11:05 +0300

    finish task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,7 @@ def search_replace(my_list, search, replace):
     if search > len(my_list):
         return my_list
     new = my_list[:]
-    new[search - 1] = replace
+    for i in range(len(new)):
+        if search == new[i]:
+            new[i] = replace
     return new
