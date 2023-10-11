d9451edcc8b89ff0df459eeef60d8602886bc801
malakehab2003 <lokaehab2003@gmail.com>
2023-10-11 21:13:04 +0300

    finish task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    roman_to_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_to_num = {"I": 1, "V": 5, "X": 10, "L": 50,
+                    "C": 100, "D": 500, "M": 1000}
     total = 0
     itr = 0
     flag = 0
@@ -11,8 +12,11 @@ def roman_to_int(roman_string):
         if flag == 1:
             flag = 0
             continue
-        if itr < len(roman_string) and roman_to_num.get(roman_string[itr]) > roman_to_num.get(roman_string[i]):
-            total += int(roman_to_num.get(roman_string[itr]) - roman_to_num.get(roman_string[i]))
+        if itr < len(roman_string)
+        and roman_to_num.get(roman_string[itr])
+        > roman_to_num.get(roman_string[i]):
+            total += int(roman_to_num.get(roman_string[itr])
+                         - roman_to_num.get(roman_string[i]))
             flag = 1
         else:
             total += int(roman_to_num.get(roman_string[i]))
