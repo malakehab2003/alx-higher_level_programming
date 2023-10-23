a707c890fd5fa242f499e6d7c8ae897fc6f0909e
malakehab2003 <lokaehab2003@gmail.com>
2023-10-23 16:25:49 +0300

    handle the index out of list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0:
+    if x == 0 or x < 0:
         return 0
     i = 0
     count = 0
@@ -13,5 +13,5 @@ def safe_print_list(my_list=[], x=0):
         print()
         return count
     except:
-        print()
+        print(0)
         return count
