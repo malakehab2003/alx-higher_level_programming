7a110c4ea0f8bb47f94fbcd258baceab6d646693
malakehab2003 <lokaehab2003@gmail.com>
2023-10-23 16:44:00 +0300

    handle the index out of list

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-my_list = [1, 2]
+my_list = []
 
 nb_print = safe_print_list(my_list, -1)
 print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,5 +13,6 @@ def safe_print_list(my_list=[], x=0):
             print("")
         return count
     except IndexError:
-        print("")
+        if i > 0:
+            print("")
         return count
