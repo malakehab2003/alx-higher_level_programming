315d982d148fda45cbe0f2ffa45c7318dc7ef5f0
malakehab2003 <lokaehab2003@gmail.com>
2023-10-23 16:34:18 +0300

    handle the index out of list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,8 +10,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             i += 1
             count += 1
-        print()
+        print("")
         return count
     except IndexError:
-        print()
+        print("")
         return count
