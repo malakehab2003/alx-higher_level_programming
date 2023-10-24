1a608b62068b1217ca34455d2456eb7e6381e680
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 17:24:39 +0300

    fix style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,10 @@ class Square:
     """class of square"""
     def __init__(self, size=0):
         self.__size = size
-        if type(self.__size) != int:
+        if type(self.__size) is not int:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size ** 2
