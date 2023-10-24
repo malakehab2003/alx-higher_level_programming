692db2925e0fd91de7c2fe9fcb30e2c311fac7a7
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 18:19:57 +0300

    handle position length

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,10 +52,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
