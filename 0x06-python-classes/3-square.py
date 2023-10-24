c69ce4a47f6be8ba4a8245a5b13d00c9c12c43ea
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 17:22:54 +0300

    add area function

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""make a new class Square"""
+
+
+class Square:
+    """class of square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size ** 2
