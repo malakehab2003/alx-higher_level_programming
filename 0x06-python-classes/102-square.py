1c2a1a29fc5ad93f3e756ddbf2819cca66f19191
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 20:28:00 +0300

    compare two squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""make a new class Square"""
+
+
+class Square:
+    """class of square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
