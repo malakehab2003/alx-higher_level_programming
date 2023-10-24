a491d1779ea2600d8de05457fdd1582bc5ee99e5
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 18:22:00 +0300

    handle position length

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,12 +11,6 @@ class Square:
         if self.__size < 0:
             raise ValueError("size must be >= 0")
         self.__position = position
-        if type(self.__position[0]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(self.__position[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if self.__position[0] < 0 or self.__position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         return self.__size ** 2
