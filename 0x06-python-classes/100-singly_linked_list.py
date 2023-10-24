c97177fb695d5646a130ed3eec15bb9578eebc58
malakehab2003 <lokaehab2003@gmail.com>
2023-10-24 20:02:34 +0300

    single linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Defines the classes Node and SinglyLinkedList"""
+
+
+class Node:
+    """
+    Class that defines properties Node.
+
+    Attributes:
+        data: data field of node.
+    """
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    Class that defines properties of SinglyLinkedList.
+
+    Attributes:
+        head: head of the SinglyLinkedList.
+    """
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        temp_var = self.__head
+        print_node = []
+        while temp_var:
+            print_node.sort()
+            print_node.append(str(temp_var.data))
+            temp_var = temp_var.next_node
+
+        print_node.sort(key=int)
+        return ("\n".join(print_node))
+
+    def sorted_insert(self, value):
+        if self.__head is None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            new_node = Node(value)
+            new_node.data = value
+            new_node.next_node = self.__head
+            self.__head = new_node
