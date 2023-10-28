780c91e40225178021b79a1e02cb19b9e0ec4455
malakehab2003 <lokaehab2003@gmail.com>
2023-10-28 18:31:33 +0300

    add and div

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" matrix divide """
+
+def matrix_divided(matrix, div):
+    l = len(matrix[0])
+    for i in range(len(matrix)):
+        if l != len(matrix[i]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) is not int and type(matrix[i][j]) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_mat = []
+    element = 0
+    for i in range(len(matrix)):
+        element = list(map(lambda x: round(x / div, 2), matrix[i]))
+        new_mat.append(element)
+
+    return new_mat
