7c3b012e8461251e256f6e14bd7c36fec622ffb5
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2023-10-30 15:28:46 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""say the name givin"""
+
+
 def say_my_name(first_name, last_name=""):
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
