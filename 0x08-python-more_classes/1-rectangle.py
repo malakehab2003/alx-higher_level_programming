66613d2f90dc9de1dc07a975fd2f9feb9f635bdc
malakehab2003 <lokaehab2003@gmail.com>
2023-10-30 19:02:34 +0300

    add width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,18 @@
 class Rectangle:
     """rectangle"""
     def __init__(self, width=0, height=0):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = height
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
         self.__width = width
 
     @property
