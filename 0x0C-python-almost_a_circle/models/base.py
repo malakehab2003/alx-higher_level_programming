2658b783ac4646c7fe5cfe1b48b8b78471c051d4
malakehab2003 <lokaehab2003@gmail.com>
2023-11-12 01:29:08 +0200

    add json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """make Base class"""
+import json
 
 
 class Base:
@@ -13,3 +14,31 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list_d = []
+        if not list_objs:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_d.append(list_objs[i].to_dictionary())
+        the_list = cls.to_json_string(list_d)
+        the_file = cls.__name__ + ".json"
+        with open(the_file, 'w') as file:
+            file.write(the_list)
+
+    @staticmethod
+    def from_json_string(json_string):
+        the_list = []
+        if json_string is None or not json_string:
+            return the_list
+
+        the_list = json.loads(json_string)
+        return the_list
