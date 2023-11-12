4df600b37225f14ea983a37cbb0a95ab31f59288
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2023-11-12 18:54:48 +0200

    add description square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,22 +6,27 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """the square class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """get the attrs"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """print data of square"""
         sqr_str = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
         return sqr_str
 
     @property
     def size(self):
+        """get the size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """set the size"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """update the attrs when added"""
         if args:
             attrs = ["id", "size", "x", "y"]
             for i in range(len(args)):
@@ -31,6 +36,7 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """get attrs as dict"""
         attrs = ["id", "size", "x", "y"]
         new_dict = {}
         for i in attrs:
