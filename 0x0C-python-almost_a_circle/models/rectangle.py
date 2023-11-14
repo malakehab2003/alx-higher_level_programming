21af207eedccc907b62f2485bb912d47aaa6fb5a
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2023-11-14 10:10:33 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """private the data"""
         if type(width) is not int:
