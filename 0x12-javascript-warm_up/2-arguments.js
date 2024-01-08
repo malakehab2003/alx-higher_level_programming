5b500c447330e10c3071ac8aafd8c3bacdc96200
malakehab2003 <lokaehab2003@gmail.com>
2024-01-08 23:36:10 +0200

    count arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,15 +2,10 @@
 
 const num = process.argv.length - 2;
 
-if (num === 1)
-{
-	console.log('Argument found');
-}
-else if (num === 0)
-{
-	console.log('No argument');
-}
-else
-{
-	console.log('Arguments found');
+if (num === 1) {
+  console.log('Argument found');
+} else if (num === 0) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
 }
