ab239e5b7d3566e4ae41f7f66f1462bbaa515c46
malakehab2003 <lokaehab2003@gmail.com>
2024-01-09 00:50:33 +0200

    first recursive function

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+const converted = parseInt(x);
+
+console.log(factorial(converted));
+
+function factorial (n) {
+  if (n === 1 || isNaN(n)) {
+    return 1;
+  }
+  else {
+    return n * factorial(n - 1);
+  }
+}
