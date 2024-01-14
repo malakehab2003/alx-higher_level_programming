9673faf87079085e24f9659ad7a000d2093efcda
malakehab2003 <lokaehab2003@gmail.com>
2024-01-14 18:19:51 +0200

    get data from database and print

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" get states of database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host = "localhost",
+        port = 3306,
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        db = sys.argv[3])
+    mycursor = con.cursor()
+
+    mycursor.execute("SELECT * FROM states")
+    data = mycursor.fetchall()
+    for row in data:
+        print(row)
+    mycursor.close()
+    con.close()
