8e8cf083292036493c9e4914ca10ad555e9efe16
malakehab2003 <lokaehab2003@gmail.com>
2024-01-15 00:26:49 +0200

    insert data in database by sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" get states of database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new = State(name="Louisiana")
+    session.add(new)
+    for item in session.query(State):
+        if item.name == "Louisiana":
+            print(item.id)
+    session.commit()
