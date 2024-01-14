1053f8107df0fdc056de71816ed276b4640bc6e3
malakehab2003 <lokaehab2003@gmail.com>
2024-01-15 00:53:15 +0200

    join two tables

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" make state class"""
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class City(Base):
+    """ the city class to table of db"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True,
+                unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
