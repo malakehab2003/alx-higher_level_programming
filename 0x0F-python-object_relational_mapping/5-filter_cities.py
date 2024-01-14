cc876205a3f761e539daf60a2d4eff5927427bd5
malakehab2003 <lokaehab2003@gmail.com>
2024-01-14 20:29:00 +0200

    join two tables

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,9 +17,9 @@ if __name__ == "__main__":
         FROM cities \
         JOIN states \
         ON cities.state_id = states.id \
-        WHERE states.name LIKE '{}' \
+        WHERE states.name LIKE %s \
         ORDER BY cities.id".format(sys.argv[4])
-    mycursor.execute(query)
+    mycursor.execute(query, (sys.argv[4],))
     data = mycursor.fetchall()
     tmp = list(i[0] for i in data)
     for item in tmp:
