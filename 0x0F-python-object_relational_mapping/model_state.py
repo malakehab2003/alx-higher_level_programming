2b21ebdc4b87b56922d77b391abd08be759fca94
malakehab2003 <lokaehab2003@gmail.com>
2024-01-14 21:26:39 +0200

    make table by sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ class State(Base):
     """ the state class to table of db"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True,
-        nullable=False, autoincrement=True,
-        unique=True)
+                nullable=False, autoincrement=True,
+                unique=True)
     name = Column(String(128), nullable=False)
