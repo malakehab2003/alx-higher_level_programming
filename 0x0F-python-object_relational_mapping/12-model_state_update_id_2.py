8cbb171dcf2313a67b20b8379f428b4365af0aa0
malakehab2003 <lokaehab2003@gmail.com>
2024-01-15 00:29:56 +0200

    update data and commit

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" get states of database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for data in session.query(State).order_by(State.id):
+        if data.id == 2:
+            data.name = "New Mexico"
+
+    session.commit()
