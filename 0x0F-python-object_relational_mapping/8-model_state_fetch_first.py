a9e40733d225b8f9ffbe668e7481cb9276f73770
malakehab2003 <lokaehab2003@gmail.com>
2024-01-14 23:40:01 +0200

    print first state id

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     session = Session()
 
     data = session.query(State).first()
-    if data == None:
+    if data is None:
         data = "Nothing"
     else:
         data = f"{data.id}: {data.name}"
