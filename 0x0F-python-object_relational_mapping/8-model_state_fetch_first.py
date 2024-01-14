3ff06db682c093f0cec8c7ca4da9ccb5f58d8da0
malakehab2003 <lokaehab2003@gmail.com>
2024-01-14 23:31:46 +0200

    print first state id

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" get states of database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    flag = 0
+    for data in session.query(State).order_by(State.id):
+        print(data.id, data.name, sep=": ")
+        flag = 1
+        break
+    if flag == 0:
+        print("Nothing")
