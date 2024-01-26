f5d10b3a30cc7bf5ec9ca85e421870031e84350d
malakehab2003 <lokaehab2003@gmail.com>
2024-01-26 23:05:48 +0200

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ import urllib.request
 
 if __name__ == "__main__":
     """ print format of url data"""
-    req = urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
-    data = req.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
-    print("\t- utf8 content: {}".format(data.decode('UTF-8')))
+    requset = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as req:
+        data = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('UTF-8')))
