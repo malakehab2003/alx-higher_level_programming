668b45ccca811100f9860811f9fd0214095a4c8c
malakehab2003 <lokaehab2003@gmail.com>
2024-01-26 23:27:15 +0200

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable"""
+""" takes in a URL, sends a request to the URL"""
 import urllib.request
 import sys
 
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter, and displays the body of the response"""
+
