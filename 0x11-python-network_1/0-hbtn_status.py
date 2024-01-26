c1087a93f711fd87fc94aa21a04d78d2731c5d6a
malakehab2003 <lokaehab2003@gmail.com>
2024-01-26 23:01:10 +0200

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+if __name__ == "__main__":
+    req = request.urlopen("https://alx-intranet.hbtn.io/status")
+    data = req.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode('UTF-8')))
