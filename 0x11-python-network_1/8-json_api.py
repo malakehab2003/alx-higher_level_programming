96898ec98058e3518bad5db467f2ff1b8785390d
root <lokaehab2003@gmail.com>
2024-01-26 16:53:47 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    param = {"q": ""}
+    if len(sys.argv) >= 2:
+        param = {'q': sys.argv[1]}
+    r = requests.post("http://0.0.0.0:5000/search_user", data = param)
+    try:
+        req = r.json()
+        if req == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get("id"), req.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
