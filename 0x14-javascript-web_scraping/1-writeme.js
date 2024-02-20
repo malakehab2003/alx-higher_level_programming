2af8576c82b4f5e3ef9cf29c9c77c3a890a338dc
malakehab2003 <lokaehab2003@gmail.com>
2024-02-21 00:09:53 +0200

    write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* write a string in a file */
+
+const fs = require('fs');
+
+const x = process.argv;
+if (x.length !== 4) {
+  console.log('usage: appname + filepath + text');
+  process.exit(1);
+}
+
+const file = x[2];
+
+fs.writeFile(file, x[3], 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("");
+  }
+});
