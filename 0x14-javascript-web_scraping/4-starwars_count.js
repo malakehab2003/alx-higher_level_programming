e5cf6a188aa15239f1b278e7b6e504ac55948d54
malakehab2003 <lokaehab2003@gmail.com>
2024-02-21 01:38:56 +0200

    count number of movies of Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,7 +13,7 @@ const url = x[2];
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
-  } else {
+  } else if (response.statusCode === 200) {
     let count = 0;
     const jsonRes = JSON.parse(body).results;
     for (const index in jsonRes) {
