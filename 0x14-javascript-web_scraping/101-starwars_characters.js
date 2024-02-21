ae652e50733c197743126a14da589c586a627a41
malakehab2003 <lokaehab2003@gmail.com>
2024-02-21 03:08:21 +0200

    get all chars name of a movie sorted

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,31 +1,20 @@
 #!/usr/bin/node
-/* get names of all chars in a movie */
 const request = require('request');
-
-const x = process.argv;
-if (x.length !== 3) {
-  console.log('usage: appname + id');
-  process.exit(1);
-}
-
-const id = x[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const jsonRes = JSON.parse(body);
-    for (const i in jsonRes.characters) {
-      const chars = jsonRes.characters;
-      request(chars[i], (error, response, data) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const people = JSON.parse(data);
-          console.log(people.name);
-        }
-      });
-    }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
